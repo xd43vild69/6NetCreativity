e7be2c63cbccb07025a062ec068d8d7a4a557ebe
xd43vild69 <xd43vild69@gmail.com>
2021-11-25 21:47:50 -0500

    git ignore

--- 6NetConsole/Program.cs
+++ 6NetConsole/Program.cs
@@ -2,5 +2,5 @@
 // See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-Domain d = new Domain();
+Domain d = new Domain(1);
 d.LoadDataConfiguration();
