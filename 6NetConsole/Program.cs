2cb541a9a187ee141c4a94d0e297b73220a76282
xd43vild69 <xd43vild69@gmail.com>
2021-11-25 21:44:21 -0500

    basic structure

--- 6NetConsole/Program.cs
+++ 6NetConsole/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NetConsole;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Domain d = new Domain();
+d.LoadDataConfiguration();
