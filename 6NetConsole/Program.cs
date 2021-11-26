1538538a4d2a2f64d5dd052ef2731544822290da
xd43vild69 <xd43vild69@gmail.com>
2021-11-26 09:37:17 -0500

    clean code

--- 6NetConsole/Program.cs
+++ 6NetConsole/Program.cs
@@ -1,6 +1,4 @@
 ﻿using NetConsole;
-// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
 
 Domain d = new Domain(1);
 d.ManagerDomain();
--- 6NetConsole/Quote.cs
+++
@@ -1,9 +0,0 @@
-namespace NetConsole;
-public class Quote
-{ 
-    public int QuoteId { get; set; }
-    public string? Value { get; set; }
-    public string? AutorId { get; set; }
-    public string? QuoteParentId { get; set; }
-    public string? QuoteSonId { get; set; }
-}
